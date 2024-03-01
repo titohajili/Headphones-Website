b84f9af382c64b8ac9c5a33c20fd8f507265c88c
titohajili <maqatoto963@gmail.com>
2024-03-01 19:06:58 +0400

    Scroll Bar & Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,12 @@ let swiperFavorite = new Swiper('.favorite__swiper', {
         }
     }
 })
+
+/*========== SCROLL UP ==========*/
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
