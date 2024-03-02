c0fe7cd49697731ce890e5189b30ff1c4952a595
titohajili <maqatoto963@gmail.com>
2024-03-02 12:13:25 +0400

    SCROLL SCTIONS ACTIVE LINK

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,26 @@ const scrollUp = () =>{
                         : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+/*=========== SCROLL SCTIONS ACTIVE LINK ==========*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
